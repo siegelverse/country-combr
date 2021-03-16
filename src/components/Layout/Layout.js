9e8d6fa3f3124334f0c4ac2973b212e1cd771ab0
siegelverse <msiegel37@gmail.com>
2021-03-16 14:25:23 -0500

    added country table & search component/styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import styles from './Layout.module.css';
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title = 'CountryCombr' }) => {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
+                <title>{title}</title>
+                <link rel="icon" href="/favicon.png" />
             </Head>
 
             <header className={styles.header}>
@@ -18,7 +18,7 @@ const Layout = ({ children }) => {
             </main>
     
             <footer className={styles.footer}>
-                footer
+                <a href='http://www.marcus-siegel.dev'>Marcus Siegel</a>
             </footer>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,26 @@
 import Head from 'next/head'
+import CountriesTable from '../components/CountriesTable/CountriesTable';
 import Layout from '../components/Layout/Layout'
+import SearchInput from '../components/SearchInput/SearchInput';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return <Layout>main</Layout>
+export default function Home({ countries }) {
+  console.log(countries);
+  return <Layout>
+    <div className={styles.counts}>Found {countries.length} countries</div>
+
+    <SearchInput placeholder='Filter by Name, Region, or Subregion' />
+    <CountriesTable countries={countries} />
+  </Layout>
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    }
+  }
 }
